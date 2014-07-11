8aa855b6bf63d6254f178d3f830235b66cf34234
Nate Otto <nate@ottonomy.net>
2014-07-10 20:33:24 -0700

    put a quick script to bake assertions into images on index.js.

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-const preloadedSchema = {
-	'ageTarget':'/Users/nate/www/badgeverity/testfiles/schema-age-target-v0.5.json',
-	'badgeClass': '../testfiles/badgeClass.json'
-};
+const oven = require('openbadges-bakery');
+const fs = require('fs');
 
-const ls = require('./lib/loadSchema');
-ls.setStorage(preloadedSchema);
+var thejson = {"uid":"198001222fc99c49bb86607de9e2504d585746a0","recipient":{"type":"email","hashed":true,"salt":"kosha","identity":"sha256$a20a52da552b1b130e95d4d0b8b6bbd1ce88bbf3f4ce94f1a9ff349d1f1ccef6"},"badge":{"name":"Open edX Contributor","description":"This badge is awarded for contributions to the Open edX codebase, typically through an accepted pull request on GitHub. Earners of this badge tend to be public-spirited hackers willing to dig into a complex codebase. They know their way around version control and participation in open source communities.","image":"http://fierce-ridge-5067.herokuapp.com//images/badge/21","criteria":"http://fierce-ridge-5067.herokuapp.com//system/good/badge/open-edx-contributor/criteria","issuer":{"name":"good","url":"http://example.com/nice","description":"yup good","_location":"http://young-basin-5816.herokuapp.com/public/systems/good","origin":"http://example.com"},"_location":"http://young-basin-5816.herokuapp.com/public/systems/good/badges/open-edx-contributor"},"verify":{"url":"http://young-basin-5816.herokuapp.com/public/assertions/198001222fc99c49bb86607de9e2504d585746a0","type":"hosted"},"issuedOn":1399399583,"_originalRecipient":{"identity":"sha256$356e4c51f2ea47db376926d44c8a47ba91064239c737c92a4acff629c52edd17","type":"email","hashed":true},"issued_on":1399399583};
+var inputFile = fs.readFileSync('./test/testfiles/dddd.png');
+var outputPath = './test/testfiles/test.png';
 
-function getSchema(identifier, callback){
-	console.log("In Index.js - Trying to load schema: " + identifier);
-	var result; 
-	ls.loadSchema(identifier,function(err, data){
-		if (err)
-			callback(err);
-		else {
-			console.log("In Index.js callback - found schema of type " + typeof foundSchema); 
-			callback(null, foundSchema);
-		}
-	});
-}
+function bakeImage(inputImg, assertionToBake, outputFilename,)
+oven.bake({image: inputImg, assertion: thejson}, function(err, imageData){
+	if (!err) {
+		fs.writeFileSync(outputFilename,imageData);
+		console.log("we think it is a success.");
+	}
+	else {
+		console.log("we think it is a failure")
+		console.log(err);
+	}
+});
 
-
-
-module.exports.getSchema = getSchema;
+// bakeImage(inputFile,thejson,outputPath);
